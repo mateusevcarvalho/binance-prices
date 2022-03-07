4a7db9333357b716fdcde506432d8b36c51c1cec
Mateus Carvalho <mateus@iMac-de-Mateus.local>
2022-03-07 14:13:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/get-prices', async (req, res) => {
+    const params = req.query;
+    const formDataParams = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+
+    try {
+        const response = await axios.get('https://api.binance.com/api/v3/ticker/price?' + formDataParams);
+        return res.json(response.data)
+    } catch (e) {
+        res.status(e.status).send(e);
+    }
+});
+
+
+app.listen(4001);
